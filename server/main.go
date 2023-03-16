0ae9ce35608e43d8807487be31336ca3d0582758
DamirBashirov <dam.bashirov@gmail.com>
2023-03-16 16:23:32 +0300

    add run grpc server

--- server/main.go
+++ server/main.go
@@ -4,16 +4,26 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
+	"os"
+	"os/signal"
+	"time"
 
-	"github.com/dbashirov/grpc-tasks/api"
 	pb "github.com/dbashirov/grpc-tasks/api"
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/reflection"
 	"google.golang.org/grpc/status"
 )
 
-var collection *mongo.Collection
+var (
+	defaultPort = "8080"
+	collection  *mongo.Collection
+)
 
 type task struct {
 	id   primitive.ObjectID `bson:"_id,omitempty"`
@@ -48,14 +58,14 @@ func (s *server) CreateTask(ctx context.Context, req *pb.CreateTaskRequest) (*pb
 	if !ok {
 		return nil, status.Errorf(
 			codes.Internal,
-			fmt.Sprintf("Connot convert to OID"),
+			fmt.Sprint("Cannot convert to OID"),
 		)
 	}
 
 	log.Println("End create task")
 
-	return &api.CreateTaskResponse{
-		Task: &api.Task{
+	return &pb.CreateTaskResponse{
+		Task: &pb.Task{
 			Id:   oid.Hex(),
 			Name: t.GetName(),
 			Desc: t.GetDesc(),
@@ -66,4 +76,59 @@ func (s *server) CreateTask(ctx context.Context, req *pb.CreateTaskRequest) (*pb
 
 func main() {
 
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	err := godotenv.Load("env_test.env")
+	if err != nil {
+		log.Fatal("Error loading .env files")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	mongoURL := os.Getenv("MONGODB_URL")
+
+	log.Println("Connect to MongoDB")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoURL))
+	if err != nil {
+		log.Fatal(err)
+	}
+	collection = (*mongo.Collection)(client.Database("taskdb").Collection("task"))
+
+	log.Println("Task service started")
+	s := grpc.NewServer()
+	pb.RegisterTaskServiceServer(s, &server{})
+
+	lis, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go func() {
+		log.Println("Starting server...")
+		if err := s.Serve(lis); err != nil {
+			log.Fatalf("Failed to serv: %v\n", err)
+		}
+	}()
+
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+
+	<-ch
+
+	log.Println("Closing MongoDB connection")
+	if err := client.Disconnect(context.Background()); err != nil {
+		log.Fatalf("Error on disconnection with MongoDB: %v\n", err)
+	}
+
+	log.Println("Stoping server")
+	s.Stop()
+	log.Println("End of Program")
+
+	reflection.Register(s)
 }
